140f1a41a513449961e39839d4ee029372cee422
Naman Kumar <namankumar0727@gmail.com>
2026-03-08 16:03:31 +0530

    [Naman] : Initial Setup Address Book System

---
+++ src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
@@ -0,0 +1,16 @@
+package com.naman.addressbooksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSystemApplication.class, args);
+	
+		System.out.println("Welcome to Address Book Program");
+		
+	}
+
+}
