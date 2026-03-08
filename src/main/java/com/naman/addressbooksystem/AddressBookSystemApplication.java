05ca5be8493aada712e26719bb3f19b9e14290d8
Naman Kumar <namankumar0727@gmail.com>
2026-03-08 17:01:23 +0530

    [Naman] : Added UC2, Add Contact

--- src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
+++ src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
@@ -4,26 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AddressBookSystemApplication {
-
+public class AddressBookSystemApplication{
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookSystemApplication.class, args);
-	
 		System.out.println("Welcome to Address Book Program");
+		System.out.println("--------------------------------");
 		
 		AddressBook addressBook = new AddressBook();
 
-        ContactPerson person = addressBook.createContact();
+        addressBook.addContact();
 
-        System.out.println("\nContact Created Successfully!");
-
-        System.out.println(person.firstName + " " + person.lastName);
-        System.out.println(person.address);
-        System.out.println(person.city + ", " + person.state);
-        System.out.println("Zip: " + person.zip);
-        System.out.println("Phone: " + person.phoneNumber);
-        System.out.println("Email: " + person.email);
-		
+        addressBook.displayContacts();
+        
+		//SpringApplication.run(AddressBookAppMain.class, args);
 	}
 
 }
