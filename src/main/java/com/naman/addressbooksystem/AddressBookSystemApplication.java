c18547285588efaa382bf4851669ea2924d4b8a8
Naman Kumar <namankumar0727@gmail.com>
2026-03-08 19:45:05 +0530

    [Naman] : Added UC4, Delete Contact

--- src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
+++ src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
@@ -11,6 +11,8 @@ public class AddressBookSystemApplication {
         addressBook.addContact();
         
         addressBook.editContact();
+        
+        addressBook.deleteContact();
 
         addressBook.displayContacts();
         
